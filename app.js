f88ff13232dd9f30ef7ff89e5846c1c41d9d2c5c
terrymcquerry <160494667+terrymcquerry@users.noreply.github.com>
2024-05-14 15:17:33 +0330

    added computer choice func

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("hi")
+function getComputerChoice() {
+    let number = Math.floor(Math.random() * 3);
+    if (number == 0) {return "rock"}
+    if (number == 1) {return "paper"}
+    if (number == 2) {return "scissors"}
+}
+
