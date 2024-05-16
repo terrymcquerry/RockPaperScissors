068357f61dfb1fe64763ce01ba40f53c9176f93f
terrymcquerry <160494667+terrymcquerry@users.noreply.github.com>
2024-05-16 17:56:36 +0330

    rough sketch of round and game

--- app.js
+++ app.js
@@ -6,8 +6,50 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("rock, paper, or scissors")
-    if (humanChoice.toLowerCase() == "rock") {return "rock"}
-    if (humanChoice.toLowerCase() == "paper") {return "paper"}
-    if (humanChoice.toLowerCase() == "scissors") {return "scissors"}
+    let promptChoice = prompt("rock, paper, or scissors?")
+    if (promptChoice.toLowerCase() == "rock") {return "rock"}
+    if (promptChoice.toLowerCase() == "paper") {return "paper"}
+    if (promptChoice.toLowerCase() == "scissors") {return "scissors"}
 }
+
+
+
+function playGame() {
+let humanScore = 0
+let computerScore = 0
+for (let i = 0; i < 5; i++) {
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice)}
+function playRound(humanChoice, computerChoice) {
+if (humanChoice == "rock" && computerChoice == "paper"){
+console.log("you lose!"),
+computerScore++}
+if (humanChoice == "rock" && computerChoice == "scissors"){
+console.log("you win!"),
+humanScore++}
+if (humanChoice == "rock" && computerChoice == "rock"){
+console.log("it's a tie!")}
+if (humanChoice == "paper" && computerChoice == "rock"){
+console.log("you win!"),
+humanScore++}
+if (humanChoice == "paper" && computerChoice == "paper"){
+console.log("it's a tie!")}
+if (humanChoice == "paper" && computerChoice == "scissors"){
+console.log("you lose!"),
+computerScore++}
+if (humanChoice == "scissors" && computerChoice == "rock"){
+console.log("you win!"),
+humanScore++}
+if (humanChoice == "scissors" && computerChoice == "paper"){
+console.log("you lose!"),
+computerScore++}
+if (humanChoice == "scissors" && computerChoice == "scissors"){
+console.log("it's a tie!")}
+}
+console.log(humanScore, computerScore)
+}
+
+
+
+playGame();
